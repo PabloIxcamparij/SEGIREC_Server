f16ae967ccf047632bfd396816d5630109f29785
IxcamJose <103219251+IxcamJose@users.noreply.github.com>
2025-09-25 01:43:04 -0600

    feat: actualizar URL de base de datos de desarrollo y mejorar la configuración de Sequelize

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,21 +1,30 @@
 import { Sequelize } from "sequelize-typescript";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 // Elige la URL de la base de datos basándose en el entorno
-const dbUrl = process.env.NODE_ENV === 'production' 
-  ? process.env.DATABASE_URL_PROD
-  : process.env.DATABASE_URL_DEV;
+const dbUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL_PROD
+    : process.env.DATABASE_URL_DEV;
 
 if (!dbUrl) {
-  throw new Error("La URL de la base de datos no está definida. Por favor, revisa tus variables de entorno.");
+  throw new Error(
+    "La URL de la base de datos no está definida. Por favor, revisa tus variables de entorno."
+  );
 }
 
 const db = new Sequelize(dbUrl, {
-  models: [__dirname + '/../models/**/*'],
+  models: [__dirname + "/../models/**/*"],
   logging: false,
-  dialectOptions: {}
+  // ESTO ES LO CORRECTO: anidar la configuración SSL
+  dialectOptions: {
+    ssl: {
+      require: true, // Forzar el uso de SSL
+      rejectUnauthorized: false, // Ignorar la verificación del certificado (común en Render, Heroku, etc.)
+    },
+  },
 });
 
 export default db;
