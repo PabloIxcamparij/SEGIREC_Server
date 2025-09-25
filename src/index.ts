ca86fd4c391fc468e4816880168f9f074c4f4edc
IxcamJose <103219251+IxcamJose@users.noreply.github.com>
2025-09-25 02:14:18 -0600

    Revert "feat: agregar configuración de Vercel y archivo .vercelignore"
    This reverts commit 751fd107964326fbea038fb0644aacc1b38e86fc.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
-import server from "./server";
-
-// Exporta la instancia de Express para que Vercel la pueda usar.
-// El puerto y el listen() son manejados automáticamente por Vercel.
-export default server; 
-
-// Si aún quieres mantener la funcionalidad de desarrollo local:
-/*
+import server from "./server"
 import colors from "colors"
-if (process.env.NODE_ENV !== 'production') {
-    const port = process.env.PORT || 4000
-    server.listen(port, () =>{
-        console.log(colors.cyan(`Rest API in the port ${port}`))
-    })
-}
-*/
+
+const port = process.env.PORT || 4000
+server.listen(port, () =>{
+ console.log(colors.cyan(`Rest API in the port ${port}`))
+} )
+
