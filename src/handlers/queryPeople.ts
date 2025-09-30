9e69804f6f29ee27be34dd2631df59fa55ccc893
IxcamJose <103219251+IxcamJose@users.noreply.github.com>
2025-09-30 14:54:21 -0600

    Cambios varios

--- src/handlers/queryPeople.ts
+++ src/handlers/queryPeople.ts
@@ -18,7 +18,7 @@ export const queryPropiedadesByFilters = async (
       whereClause.NOM_DISTRI = { [Op.in]: distritos };
     }
 
-     // Filtro por área
+    // Filtro por área
     const min = areaMinima !== undefined ? Number(areaMinima) : undefined;
     const max = areaMaxima !== undefined ? Number(areaMaxima) : undefined;
 
@@ -32,13 +32,13 @@ export const queryPropiedadesByFilters = async (
     }
 
     const personas = await PropiedadesNoDeclaradas.findAll({
-    attributes: [
-        ['CEDULA', 'cedula'],
-        ['NOM_PERSON', 'nombre'],
-        ['APELLIDOS', 'apellido'],
-        ['CORREO_ELE', 'correo'],
-        ['NOM_DISTRI', 'distrito'], 
-        ['AREA_REGIS', 'areaDeLaPropiedad'],
+      attributes: [
+        ["CEDULA", "cedula"],
+        ["NOM_PERSON", "nombre"],
+        ["APELLIDOS", "apellido"],
+        ["CORREO_ELE", "correo"],
+        ["NOM_DISTRI", "distrito"],
+        ["AREA_REGIS", "areaDeLaPropiedad"],
       ],
       where: whereClause,
       raw: true,
@@ -54,20 +54,38 @@ export const queryPropiedadesByFilters = async (
 // Endpoint para consultar una persona por su cédula
 export const queryPropiedadesByCedula = async (req: Request, res: Response) => {
   try {
-    const { cedula } = req.body;
+    const { cedula, typeQuery } = req.body;
+    console.log(typeQuery);
+    let persona;
 
-    const persona = await PropiedadesNoDeclaradas.findOne({
-      attributes: [
-        ['CEDULA', 'cedula'],
-        ['NOM_PERSON', 'nombre'],
-        ['APELLIDOS', 'apellido'],
-        ['CORREO_ELE', 'correo'],
-        ['NOM_DISTRI', 'distrito'], 
-        ['AREA_REGIS', 'areaDeLaPropiedad'],
-      ],
-      where: { CEDULA: cedula },
-      raw: true,
-    });
+    if (typeQuery === "Propiedades") {
+      persona = await PropiedadesNoDeclaradas.findOne({
+        attributes: [
+          ["CEDULA", "cedula"],
+          ["NOM_PERSON", "nombre"],
+          ["APELLIDOS", "apellido"],
+          ["CORREO_ELE", "correo"],
+          ["NOM_DISTRI", "distrito"],
+          ["AREA_REGIS", "areaDeLaPropiedad"],
+        ],
+        where: { CEDULA: cedula },
+        raw: true,
+      });
+    } else if (typeQuery === "Morosidad") {
+      persona = await Morosidad.findOne({
+        attributes: [
+          ["CEDULA", "cedula"],
+          ["NOM_COMPLE", "nombre"],
+          ["CORREO_ELE", "correo"],
+          ["NOM_DISTRI", "distrito"],
+          ["DES_SERVIC", "servicio"],
+          ["MON_DEUDA", "valorDeLaDeuda"],
+          ["FEC_VENCIM", "fechaVencimiento"],
+        ],
+        where: { CEDULA: cedula },
+        raw: true,
+      });
+    }
 
     if (!persona) {
       return res.status(404).json({ error: "Persona no encontrada." });
@@ -78,10 +96,12 @@ export const queryPropiedadesByCedula = async (req: Request, res: Response) => {
     return res.status(500).json({ error: "Error interno en el servidor." });
   }
 };
+
 // Endpoint para consultar una persona por su nombre
-export const  queryPropiedadesByName = async (req: Request, res: Response) => {
+export const queryPropiedadesByName = async (req: Request, res: Response) => {
   try {
-    const { nombre } = req.body;
+    const { nombre, typeQuery } = req.body;
+    let personas;
 
     if (!nombre) {
       return res
@@ -91,38 +111,63 @@ export const  queryPropiedadesByName = async (req: Request, res: Response) => {
 
     const palabras = nombre.trim().split(/\s+/);
 
-    const condiciones = palabras.map((palabra: string) => ({
-      [Op.or]: [
-        Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("NOM_PERSON")), {
-          [Op.like]: `%${palabra.toLowerCase()}%`,
-        }),
-        Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("SEG_NOMBRE")), {
-          [Op.like]: `%${palabra.toLowerCase()}%`,
-        }),
-        Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("APELLIDOS")), {
-          [Op.like]: `%${palabra.toLowerCase()}%`,
-        }),
-        Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("SEG_APELLI")), {
-          [Op.like]: `%${palabra.toLowerCase()}%`,
-        }),
-      ],
-    }));
+    if (typeQuery === "Propiedades") {
+      const condiciones = palabras.map((palabra: string) => ({
+        [Op.or]: [
+          Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("NOM_PERSON")), {
+            [Op.like]: `%${palabra.toLowerCase()}%`,
+          }),
+          Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("SEG_NOMBRE")), {
+            [Op.like]: `%${palabra.toLowerCase()}%`,
+          }),
+          Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("APELLIDOS")), {
+            [Op.like]: `%${palabra.toLowerCase()}%`,
+          }),
+          Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("SEG_APELLI")), {
+            [Op.like]: `%${palabra.toLowerCase()}%`,
+          }),
+        ],
+      }));
 
-    const personas = await PropiedadesNoDeclaradas.findAll({
-      attributes: [
-       ['CEDULA', 'cedula'],
-        ['NOM_PERSON', 'nombre'],
-        ['APELLIDOS', 'apellido'],
-        ['CORREO_ELE', 'correo'],
-        ['NOM_DISTRI', 'distrito'], 
-        ['AREA_REGIS', 'areaDeLaPropiedad'],
-      ],
-      where: {
-        [Op.and]: condiciones,
-      },
-      raw: true,
-    });
+      personas = await PropiedadesNoDeclaradas.findAll({
+        attributes: [
+          ["CEDULA", "cedula"],
+          ["NOM_PERSON", "nombre"],
+          ["APELLIDOS", "apellido"],
+          ["CORREO_ELE", "correo"],
+          ["NOM_DISTRI", "distrito"],
+          ["AREA_REGIS", "areaDeLaPropiedad"],
+        ],
+        where: {
+          [Op.and]: condiciones,
+        },
+        raw: true,
+      });
+    } else if (typeQuery === "Morosidad") {
+      const condiciones = palabras.map((palabra: string) => ({
+        [Op.or]: [
+          Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("NOM_COMPLE")), {
+            [Op.like]: `%${palabra.toLowerCase()}%`,
+          }),
+        ],
+      }));
 
+      personas = await Morosidad.findAll({
+        attributes: [
+          ["CEDULA", "cedula"],
+          ["NOM_COMPLE", "nombre"],
+          ["CORREO_ELE", "correo"],
+          ["NOM_DISTRI", "distrito"],
+          ["DES_SERVIC", "servicio"],
+          ["MON_DEUDA", "valorDeLaDeuda"],
+          ["FEC_VENCIM", "fechaVencimiento"],
+        ],
+        where: {
+          [Op.and]: condiciones,
+        },
+        raw: true,
+      });
+    }
     if (!personas || personas.length === 0) {
       return res.status(404).json({ error: "No se encontraron personas." });
     }
@@ -135,9 +180,13 @@ export const  queryPropiedadesByName = async (req: Request, res: Response) => {
 };
 
 // Endpoint para consultar las personas por medio de un archivo
-export const queryPropiedadesByArchive = async (req: Request, res: Response) => {
+export const queryPropiedadesByArchive = async (
+  req: Request,
+  res: Response
+) => {
   try {
-    const { cedulas } = req.body;
+    const { cedulas, typeQuery } = req.body;
+    let personas;
 
     // Verificar que el body contenga un array de cédulas
     if (!Array.isArray(cedulas) || cedulas.length === 0) {
@@ -145,23 +194,42 @@ export const queryPropiedadesByArchive = async (req: Request, res: Response) => 
         .status(400)
         .json({ error: "Debe proporcionar un array de cédulas." });
     }
-
-    const personas = await PropiedadesNoDeclaradas.findAll({
-      attributes: [
-       ['CEDULA', 'cedula'],
-        ['NOM_PERSON', 'nombre'],
-        ['APELLIDOS', 'apellido'],
-        ['CORREO_ELE', 'correo'],
-        ['NOM_DISTRI', 'distrito'], 
-        ['AREA_REGIS', 'areaDeLaPropiedad'],
-      ],
-      where: {
-        CEDULA: {
-          [Op.in]: cedulas,
+    if (typeQuery === "Propiedades") {
+      personas = await PropiedadesNoDeclaradas.findAll({
+        attributes: [
+          ["CEDULA", "cedula"],
+          ["NOM_PERSON", "nombre"],
+          ["APELLIDOS", "apellido"],
+          ["CORREO_ELE", "correo"],
+          ["NOM_DISTRI", "distrito"],
+          ["AREA_REGIS", "areaDeLaPropiedad"],
+        ],
+        where: {
+          CEDULA: {
+            [Op.in]: cedulas,
+          },
         },
-      },
-      raw: true,
-    });
+        raw: true,
+      });
+    } else if (typeQuery === "Morosidad") {
+      personas = await Morosidad.findAll({
+        attributes: [
+          ["CEDULA", "cedula"],
+          ["NOM_COMPLE", "nombre"],
+          ["CORREO_ELE", "correo"],
+          ["NOM_DISTRI", "distrito"],
+          ["DES_SERVIC", "servicio"],
+          ["MON_DEUDA", "valorDeLaDeuda"],
+          ["FEC_VENCIM", "fechaVencimiento"],
+        ],
+        where: {
+          CEDULA: {
+            [Op.in]: cedulas,
+          },
+        },
+        raw: true,
+      });
+    }
 
     if (!personas || personas.length === 0) {
       return res.status(404).json({
@@ -181,6 +249,8 @@ export const queryPeopleWithDebt = async (req: Request, res: Response) => {
   try {
     const { distritos, servicios, deudaMaxima, deudaMinima } = req.body;
 
+    console.log(deudaMinima, deudaMaxima)
+    
     const whereClause: any = {};
 
     // Filtro por distritos
@@ -193,7 +263,7 @@ export const queryPeopleWithDebt = async (req: Request, res: Response) => {
       whereClause.DES_SERVIC = { [Op.in]: servicios };
     }
 
-     // Filtro por área
+    // Filtro por área
     const min = deudaMinima !== undefined ? Number(deudaMinima) : undefined;
     const max = deudaMaxima !== undefined ? Number(deudaMaxima) : undefined;
 
@@ -205,16 +275,16 @@ export const queryPeopleWithDebt = async (req: Request, res: Response) => {
     } else if (max !== undefined) {
       whereClause.MON_DEUDA = { [Op.lte]: max };
     }
-    
+
     const personas = await Morosidad.findAll({
-    attributes: [
-        ['CEDULA', 'cedula'],
-        ['NOM_COMPLE', 'nombre'],
-        ['CORREO_ELE', 'correo'],
-        ['NOM_DISTRI', 'distrito'], 
-        ['DES_SERVIC', 'servicio'],
-        ['MON_DEUDA', 'valorDeLaDeuda'],
-        ['FEC_VENCIM', 'fechaVencimiento'],
+      attributes: [
+        ["CEDULA", "cedula"],
+        ["NOM_COMPLE", "nombre"],
+        ["CORREO_ELE", "correo"],
+        ["NOM_DISTRI", "distrito"],
+        ["DES_SERVIC", "servicio"],
+        ["MON_DEUDA", "valorDeLaDeuda"],
+        ["FEC_VENCIM", "fechaVencimiento"],
       ],
       where: whereClause,
       raw: true,
@@ -226,4 +296,3 @@ export const queryPeopleWithDebt = async (req: Request, res: Response) => {
     return res.status(500).json({ error: "Error interno en el servidor." });
   }
 };
-
