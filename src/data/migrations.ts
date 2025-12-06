38ce17d6510dd73a3aa49cb54a54ea613d464aa8
PabloIxcamparij <j.pablo.sorto@gmail.com>
2025-12-05 21:17:22 -0600

    feat: Update initial migration to create admin user with correct details

--- src/data/migrations.ts
+++ src/data/migrations.ts
@@ -8,14 +8,14 @@ import Usuarios from "../models/User.model"; // ajusta la ruta según tu estruct
 export async function runInitialMigration() {
   try {
     // Buscar si ya existe un usuario administrador por correo
-    const adminEmail = "j.pablo.sorto@gmail.com";
+    const adminEmail = "roylopezs@bagaces.go.cr";
     const adminExists = await Usuarios.findOne({ where: { Correo: adminEmail } });
 
     if (!adminExists) {
       console.log("No existe el usuario administrador. Creándolo...");
 
       await Usuarios.create({
-        Nombre: "Pablo",
+        Nombre: "Roy",
         Rol: "Administrador",
         Correo: adminEmail,
         Clave: "12345678",
